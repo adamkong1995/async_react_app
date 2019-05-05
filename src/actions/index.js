0be3002ee656f6c60933c18003072a71ad116f00
adamkong1995 <adamkong256@gmail.com>
2019-05-05 15:58:58 +0800

    init redux-thunk

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// Passing a functional actions creator to react thunk
+export const fetchPosts = () => async (dispatch, getState) => {
+    const response = await jsonPlaceholder.get('/posts');
+    
+    dispatch({ type: 'FETCH_POSTS', payload: response });
+};
