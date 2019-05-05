5bd9a7cd9f6967dc25a368d786b70c2058670c4a
adamkong1995 <adamkong256@gmail.com>
2019-05-05 21:06:12 +0800

    working on 5/5

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,6 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 // Passing a functional actions creator to react thunk
 export const fetchPosts = () => async (dispatch, getState) => {
     const response = await jsonPlaceholder.get('/posts');
-    
+
     dispatch({ type: 'FETCH_POSTS', payload: response });
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import postReducer from './postReducer';
 
 export default combineReducers({
-    test: () => 'dummy reducers'
+    posts: postReducer
 });
